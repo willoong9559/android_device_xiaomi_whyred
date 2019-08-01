0eb634139ce272cb7487485cada2374ca861fa99
willoong9559 <501801418@qq.com>
2019-08-01 20:07:30 +0800

    whyred:Add basic board configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Board device path
+BOARD_VENDOR := xiaomi
+DEVICE_PATH := device/xiaomi/whyred
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a73
+
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sdm660
+TARGET_NO_BOOTLOADER := true
+
+# Inherit from proprietary files
+-include vendor/xiaomi/whyred/BoardConfigVendor.mk
