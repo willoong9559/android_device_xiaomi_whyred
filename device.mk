f64fa71a9b65697687884af5b41d7e52959b104e
willoong9559 <501801418@qq.com>
2019-08-03 11:32:59 +0800

    whyred: Build 32-bit android.frameworks.displayservice@1.0

--- device.mk
+++ device.mk
@@ -77,6 +77,13 @@ BLUETOOTH_HCI_USE_MCT := true
 
 # Camera
 PRODUCT_PACKAGES += \
+	android.frameworks.displayservice@1.0_32 \
+  android.hardware.camera.provider@2.4-impl \
+  android.hardware.camera.provider@2.4-service \
+  camera.device@3.2-impl \
+  camera.device@1.0-impl
+
+PRODUCT_PACKAGES += \
   Snap
 
 # CNE
