537f55e652ba81b1fe4f71a795af9297d53454ca
willoong9559 <501801418@qq.com>
2019-08-03 11:48:16 +0800

    whyred: Add missing Wifi configs

--- device.mk
+++ device.mk
@@ -281,5 +281,25 @@ PRODUCT_COPY_FILES += \
 	prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v27.so \
 	prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0-v27.so
 
+	# Wifi
+PRODUCT_PACKAGES += \
+	android.hardware.wifi@1.0-service \
+	hostapd \
+	hostapd_cli \
+	libqsap_sdk \
+	libQWiFiSoftApCfg \
+	libwifi-hal-qcom \
+	libwpa_client \
+	wificond \
+	wifilogd \
+	wpa_supplicant \
+	wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+	$(PLATFORM_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+	$(PLATFORM_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+	$(PLATFORM_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
