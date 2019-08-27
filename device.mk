b89b9dcf68df97379444d0d9c9f41e2a704deeeb
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-08-27 14:51:27 +0800

    whyred: add vndk sp libs

--- device.mk
+++ device.mk
@@ -366,14 +366,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-	libdng_sdk.vendor_32 \
-	libstdc++.vendor \
-	vndk-ext \
-	vndk_package
-
-PRODUCT_COPY_FILES += \
-	prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v27.so \
-	prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0-v27.so
+    vndk-sp
 
 # Wifi
 PRODUCT_PACKAGES += \
--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,46 +1,49 @@
+ifeq ($(BOARD_VNDK_VERSION),)
+$(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
 LOCAL_PATH := $(call my-dir)
 
-EXTRA_VENDOR_LIBRARIES_32 := \
-    libaudioclient \
-    libaudiomanager \
-    libbinder \
-    libbufferhubqueue \
-    libcamera_client \
-    libft2 \
-    libgui \
-    libharfbuzz_ng \
-    libheif \
-    libicui18n \
-    libicuuc \
-    libmedia \
-    libmediaextractor \
-    libmediametrics \
-    libminikin \
-    libpdx_default_transport \
-    libsonivox
+include $(LOCAL_PATH)/vndk-sp-libs.mk
 
-define define-vndk-lib
+vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
+
+define define-vndk-sp-lib
 include $$(CLEAR_VARS)
-LOCAL_MODULE := $1.vndk-ext-gen
+LOCAL_MODULE := $1.vndk-sp-gen
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,$3,)/$1.so
+LOCAL_PREBUILT_MODULE_FILE := $$(TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
 LOCAL_STRIP_MODULE := false
-LOCAL_MULTILIB := $2
+LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)
 include $$(BUILD_PREBUILT)
+
+ifneq ($$(TARGET_2ND_ARCH),)
+ifneq ($$(TARGET_TRANSLATE_2ND_ARCH),true)
+include $$(CLEAR_VARS)
+LOCAL_MODULE := $1.vndk-sp-gen
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PREBUILT_MODULE_FILE := $$($$(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
+LOCAL_STRIP_MODULE := false
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_INSTALLED_MODULE_STEM := $1.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)
+include $$(BUILD_PREBUILT)
+endif # TARGET_TRANSLATE_2ND_ARCH is not true
+endif # TARGET_2ND_ARCH is not empty
 endef
 
-$(foreach lib,$(EXTRA_VENDOR_LIBRARIES_32),\
-    $(eval $(call define-vndk-lib,$(lib),32,$(TARGET_2ND_ARCH_VAR_PREFIX))))
-
-$(foreach lib,$(EXTRA_VENDOR_LIBRARIES_64),\
-    $(eval $(call define-vndk-lib,$(lib),first,)))
+$(foreach lib,$(VNDK_SP_LIBRARIES),\
+    $(eval $(call define-vndk-sp-lib,$(lib))))
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := vndk-ext
+LOCAL_MODULE := vndk-sp
 LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES_32) $(EXTRA_VENDOR_LIBRARIES_64))
+LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
+
+vndk_sp_dir :=
+endif
